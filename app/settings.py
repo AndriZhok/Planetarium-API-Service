a296b09561379a167016acf1884714bb31c06ecf
Andriy Zhuk <andri195595@gmail.com>
2024-11-02 19:26:15 +0200

    fix SECRET_KEY, add blank lines at the end files

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)re!c^v(prn%$8)s1e!jn8o)nksr_(-62si3%&5^e*qsodywee"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
